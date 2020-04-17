f659b258e34b8e7ae5dc9100d19141abd6084e79
sardul3 <poudelsagar530@gmail.com>
2020-04-16 23:25:31 -0500

    add password strength feature

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -12,6 +12,9 @@ export class SignupComponent implements OnInit {
   isLoading = false;
   errorMessage: string = null;
   validUsername = true;
+  labelColor = 'danger';
+  labelValue = 0;
+  labelStrength = 'very weak';
 
   constructor(private authService: AuthService,
               private loadingController: LoadingController,
@@ -49,11 +52,44 @@ export class SignupComponent implements OnInit {
     this.authService.checkUsername(usernameField.value).subscribe(data => {
       if (data['message'] === 'duplicate') {
         this.validUsername = false;
-      }
-      else {
+      } else {
         this.validUsername = true;
       }
     });
+  }
+
+  passwordStrength(pass) {
+    const password = pass.value;
+    let color = 'danger';
+    let strength: string;
+    let totalScore = 0;
+    const specialCharScore = (password.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g) || []).length;
+    const lengthScore = password.length;
+    totalScore = (specialCharScore * 10) + lengthScore;
+    if ( specialCharScore >= 2) {
+        totalScore += 10;
+    }
+    if (lengthScore >= 8 ) {
+      totalScore += 10;
+    }
+
+    if (totalScore >= 50) {
+       strength = 'super secure';
+       color = 'success';
+      } else if (totalScore < 50 && totalScore >= 30) {
+        strength = 'moderate';
+        color = 'tertiary';
+      } else if (totalScore < 30 && totalScore >= 15) {
+        strength = 'weak';
+        color = 'secondary';
+      } else { strength = 'very weak';
+    }
+    this.labelValue = totalScore;
+    this.labelStrength = strength;
+    this.labelColor = color;
+
+    console.log(totalScore);
+    return [totalScore.toString(), strength];
   }
 
 }
