917c93b3954215e522422e2d9624cb6aaa89a7aa
sardul3 <poudelsagar530@gmail.com>
2020-04-22 14:14:56 -0500

    replace dist with www

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/metadata-ion'));
+app.use(express.static('./www/metadata-ion'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/metadata-ion/index.html'));
+res.sendFile(path.join(__dirname,'/www/metadata-ion/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
